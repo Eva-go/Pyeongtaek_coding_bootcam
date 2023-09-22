6084d4541f689c2b57d145e274bc8ae431a3f30d
Eva-go <jkyu6554@tukorea.ac.kr>
2023-09-22 18:01:20 +0900

    나머지파일

---
+++ Java/coding_bootcam/src/Day2/Star.java
@@ -0,0 +1,21 @@
+package Day2;
+
+public class Star {
+    public static void main(String[] args) {
+        int num=1;
+        for(int i =0; i<5; i++)
+        {
+           for(int k = 4;k>i; k--){
+               System.out.print("  ");
+           }
+           for(int j =0; j<num; j++) {
+               System.out.print("★");
+           }
+           num=num+2;
+           System.out.println();
+        }
+
+    }
+
+
+}
